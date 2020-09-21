ca7cb73c575114c25f9230466586ae0d85efa1df
Dhiaa Alshamy <dhodhof@gmail.com>
2020-09-21 19:59:43 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+// Step 1. Import react-router functions
+import { BrowserRouter as Router, Route ,NaveLink} from 'react-router-dom';
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+const Navbar = () =>
+  <div>
+    <NavLink
+      to="/"
+      /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+      exact
+      /* add styling to Navlink */
+      style={link}
+      /* add prop for activeStyle */
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Home</NavLink>
+    <NavLink
+      to="/about"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >About</NavLink>
+    <NavLink
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Login</NavLink>
+  </div>;
 const Home = () => {
   return (
     <div>
@@ -8,8 +47,38 @@ const Home = () => {
     </div>
   );
 };
+const About = () => {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+};
 
+const Login = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+};
 ReactDOM.render(
-  <Home />,
+  <Router>
+  <Navbar>
+  <Route exact path='/' component={home}/>
+  <Route exact path='/Login' component={Login}/>
+  <Route exact path='/about' component={about}/>
+  </Router>
+  </Navbar>
   document.getElementById('root')
 );
